24fcc6dff7606bc217b0a1c769663a50281f5d55
ssaumya1711 <ssaumya1711@gmail.com>
2022-01-03 20:24:52 +0530

    react-node commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [toDo,setList] = useState([]);
+  const [task,setTask] = useState("");
+  useEffect(async() => {
+    fetchTask();
+  },[])
+  
+  let fetchTask = async() => {
+    try {
+      let listData = await axios.get("http://localhost:3000/list-all-todo");
+      setList([...listData.data])
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  let handleCreate = async() => {
+    try {
+      let postData = await axios.post("http://localhost:3000/create-task",{message:task});
+      fetchTask();
+      setTask("");
+      //alert(postData.data.message);
+    } catch (error) {
+      console.log(error); 
+    }
+  }
+
+let handleChange = async(e,id) => {
+  try {
+    let updateData = await axios.put(`http://localhost:3000/update-task/${id}`,{status:e.target.checked});
+    fetchTask();
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+let handleDelete = async(id) => {
+  try {
+    await axios.delete(`http://localhost:3000/delete-task/${id}`);
+    fetchTask();
+  } catch (error) {
+    alert(error);
+  }
+}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="row">
+        <h2>To Do</h2>
+        <div className="col-lg-12">
+          <div class="input-group mb-3">
+  <input type="text" class="form-control" value = {task} onChange={e => setTask(e.target.value)} placeholder="Task..." aria-label="Recipient's username" aria-describedby="button-addon2"/>
+  <button onClick={handleCreate} class="btn btn-outline-secondary" type="button" id="button-addon2">Button</button>
+</div>
+          <ul class="list-group">
+            {
+              toDo.map((items) => {
+                return(<li class="list-group-item">
+                  <input class="form-check-input me-1" checked={items.status} type="checkbox" value="" aria-label="..." onChange={(e) => {handleChange(e,items._id)}}/>
+                  <span style={{textDecoration:items.status ? "line-through" : ""}}>{items.message}</span>
+                  <span class="badge bg-primary rounded-pill" onClick={() => {handleDelete(items._id)}}>X</span>
+                </li>)
+              })
+            }
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
